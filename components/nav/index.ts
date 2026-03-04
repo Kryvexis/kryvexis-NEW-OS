feb0a797b29a208d0d41029291fe89aaf7c63883
Kryvexis <kryvexissolutions@gmail.com>
2026-03-04 22:24:38 +0200

    Fix navItems) + unify nav-items

--- components/nav/index.ts
+++ components/nav/index.ts
@@ -1,1 +1,6 @@
-export * from './nav-items';
+// Re-export navigation helpers from components/nav.tsx.
+// This avoids import ambiguity between ./nav (folder) and ./nav.tsx (file).
+export * from "../nav";
+
+// Also export MobileNav from this folder
+export { default as MobileNav } from "./MobileNav";
